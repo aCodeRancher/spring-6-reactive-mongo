62fd6a400b9dc7ff6cd2cee689b46303df75cee8
helen <hhma@alumni.cpp.edu>
2023-05-04 15:06:46 -0400

    add CustomerRepository

--- src/main/java/guru/springframework/reactivemongo/repositories/CustomerRepsoitory.java
+++ src/main/java/guru/springframework/reactivemongo/repositories/CustomerRepsoitory.java
@@ -2,9 +2,13 @@ package guru.springframework.reactivemongo.repositories;
 
 import guru.springframework.reactivemongo.domain.Customer;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
 
 /**
  * Created by jt, Spring Framework Guru.
  */
 public interface CustomerRepsoitory extends ReactiveMongoRepository<Customer, String> {
+
 }
+
+
